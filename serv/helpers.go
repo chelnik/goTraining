d46e3ffbe45e5ad17c213e21f7241dae0dabd446
Corki Udyr <corkiudy@un-c1.kzn.21-school.ru>
2022-08-29 18:54:10 +0300

    insert into db worked

---
+++ serv/helpers.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bufio"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+)
+
+type Guestbook struct {
+	SignatureCount int
+	Signatures     []string
+}
+
+func check(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func viewHandler(writer http.ResponseWriter, request *http.Request) {
+	//message := []byte("Hi^ men!")
+	signatures := getStrings("signatures.txt")
+	html, err := template.ParseFiles("ui/view.html")
+	check(err)
+	guestbook := Guestbook{
+		SignatureCount: len(signatures),
+		Signatures:     signatures,
+	}
+	err = html.Execute(writer, guestbook)
+	check(err)
+}
+
+func getStrings(fileName string) []string {
+	var lines []string
+	file, err := os.Open(fileName)
+	if os.IsNotExist(err) {
+		return nil
+	}
+	check(err)
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	check(scanner.Err())
+	return lines
+}
--- serv/serve.go
+++ serv/serve.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"bufio"
 	"database/sql"
-	"fmt"
 	_ "github.com/lib/pq"
 	"html/template"
 	"log"
@@ -11,89 +9,39 @@ import (
 	"os"
 )
 
-//type info struct {
-//	db *sql.DB
-//}
-
-type Guestbook struct {
-	SignatureCount int
-	Signatures     []string
+type info struct {
+	db *sql.DB
 }
 
-func check(err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-func createHandler(writer http.ResponseWriter, request *http.Request) {
+//отправляем в базу данных значение из формы
+func (dbStruct *info) createHandlerNew(writer http.ResponseWriter, request *http.Request) {
 	signature := request.FormValue("signature")
-	options := os.O_WRONLY | os.O_APPEND | os.O_CREATE
-	file, err := os.OpenFile("signatures.txt", options, os.FileMode(0600))
-	check(err)
-	_, err = fmt.Fprintln(file, signature)
-	check(err)
-	err = file.Close()
-	check(err)
+
+	_, err := dbStruct.db.Exec(`INSERT INTO signatures (phrase) VALUES($1)`, signature)
+
+	if err != nil {
+		log.Println("error of insert:", signature, err)
+	}
 	http.Redirect(writer, request, "/guestbook", http.StatusFound)
 }
-
-//func createHandlerNew(writer http.ResponseWriter, request *http.Request) {
-//	signature := request.FormValue("signature")
-//	dsn := flag.String("dsn", "postgresql://corkiudy:test@127.0.0.1:5432/forProjectGo?sslmode=disable", "Название postSQL источника данных")
-//	db, err := openDB(*dsn) // инициализируем пул подключений к базе
-//	if err != nil {
-//		log.Println("Open db")
-//	}
-//	defer db.Close()
-//	_, err = db.Exec(`INSERT INTO signatures (phrase) VALUES($1)`, signature)
-//
-//	if err != nil {
-//		log.Println("go")
-//	}
-//	http.Redirect(writer, request, "/guestbook", http.StatusFound)
-//}
 func addSignatureHandler(writer http.ResponseWriter, request *http.Request) {
 	html, err := template.ParseFiles("ui/form.html")
 	check(err)
 	err = html.Execute(writer, nil)
 	check(err)
 }
-func viewHandler(writer http.ResponseWriter, request *http.Request) {
-	//message := []byte("Hi^ men!")
-	signatures := getStrings("signatures.txt")
-	html, err := template.ParseFiles("ui/view.html")
-	check(err)
-	guestbook := Guestbook{
-		SignatureCount: len(signatures),
-		Signatures:     signatures,
-	}
-	err = html.Execute(writer, guestbook)
-	check(err)
-}
 
-func getStrings(fileName string) []string {
-	var lines []string
-	file, err := os.Open(fileName)
-	if os.IsNotExist(err) {
-		return nil
-	}
-	check(err)
-	defer file.Close()
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-	check(scanner.Err())
-	return lines
-}
 func main() {
 	dsn := "postgresql://corkiudy:test@127.0.0.1:5432/forProjectGo?sslmode=disable"
 	db, err := openDB(dsn) // инициализируем пул подключений к базе
-	db.Close()
+	defer db.Close()
 
+	dbStruct := &info{
+		db: db,
+	}
 	http.HandleFunc("/guestbook", viewHandler)
 	http.HandleFunc("/guestbook/new", addSignatureHandler)
-	http.HandleFunc("/guestbook/create", createHandler)
+	http.HandleFunc("/guestbook/create", dbStruct.createHandlerNew)
 	_, err = os.Stdout.Write([]byte("server created in http://localhost:4000/guestbook"))
 	check(err)
 	err = http.ListenAndServe("localhost:4000", nil)
